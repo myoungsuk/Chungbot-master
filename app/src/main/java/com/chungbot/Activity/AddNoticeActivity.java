14c2e2b1e578d6aba595345918a57dfa244b854c
LikeNoru <stkjas4657@naver.com>
2022-12-10 15:17:57 +0900

    new files of notice add

---
+++ app/src/main/java/com/chungbot/Activity/AddNoticeActivity.java
@@ -0,0 +1,16 @@
+package com.chungbot.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.chungbot.R;
+
+public class AddNoticeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_notice);
+    }
+}
--- app/src/main/java/com/chungbot/Fragment/NoticeFragment.java
+++ app/src/main/java/com/chungbot/Fragment/NoticeFragment.java
@@ -1,5 +1,7 @@
 package com.chungbot.Fragment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.chungbot.Activity.AddNoticeActivity;
+import com.chungbot.Activity.ChatbotActivity;
 import com.chungbot.R;
 
 public class NoticeFragment extends Fragment
 {
+    Activity activity = getActivity();
 
+    private Button addButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,6 +27,18 @@ public class NoticeFragment extends Fragment
     {
         View v=inflater.inflate(R.layout.fragment_notice,container,false);
         // Inflate the layout for this fragment
+        addButton = (Button) v.findViewById(R.id.addFloatingButton);
+
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), AddNoticeActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return v;
     }
 }
